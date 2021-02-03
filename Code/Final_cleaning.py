d456ac8602889d8c1bce31504d5d1ee775a7a275
Yannik Hubrich <yannik@gridscale.io>
2021-02-03 17:52:17 +0100

    updated a mistake

--- Code/Final_cleaning.py
+++ Code/Final_cleaning.py
@@ -10,15 +10,15 @@ tw = pd.DataFrame(pd.read_csv(path))
 tw = tw.drop(tw[tw.Symbol == '[]'].index)
 tw.reset_index()
 #print(tw)
-tw.to_csv('/Users/yannikhubrich/Documents/Studium/3Semester/GitHub/FintechConsulting/twitter_and_stockdata_cleaned_final.csv')
+tw.to_csv('/Users/yannikhubrich/Documents/Studium/3Semester/GitHub/FintechConsulting/twitter_and_stockdata_cleaned_almost_final.csv')
 
 #step 2
 
-tw = pd.DataFrame(pd.read_csv('/Users/yannikhubrich/Documents/Studium/3Semester/GitHub/FintechConsulting/twitter_and_stockdata_cleaned_final.csv'))
+tw = pd.DataFrame(pd.read_csv('/Users/yannikhubrich/Documents/Studium/3Semester/GitHub/FintechConsulting/twitter_and_stockdata_cleaned_almost_final.csv'))
 
 tw = tw.drop(columns=['Unnamed: 0', 'Unnamed: 0.1', 'Unnamed: 0.1.1'])
 
-#print(tw)
+print(tw)
 list_of_dates_to_delete = []
 for i in range(len(tw)):
     date = tw.iloc[i,2]
@@ -29,13 +29,13 @@ for i in range(len(tw)):
     #print(date)
     #print(type(date))
     date = date_time_obj + dt.timedelta(days=2)
-    print(date)
+    #print(date)
     if date >= dt.date(2021,1,1):
-        print(i)
+    #    print(i)
         list_of_dates_to_delete.append(i)
 
 #print(list_of_dates_to_delete)
 #print(len(list_of_dates_to_delete))
 tw = tw.drop(index=list_of_dates_to_delete)
-tw = pd.DataFrame(pd.read_csv('/Users/yannikhubrich/Documents/Studium/3Semester/GitHub/FintechConsulting/twitter_and_stockdata_cleaned_final.csv'))
-#print(tw)
+tw = tw.to_csv('/Users/yannikhubrich/Documents/Studium/3Semester/GitHub/FintechConsulting/twitter_and_stockdata_cleaned_final.csv')
+print(tw)
